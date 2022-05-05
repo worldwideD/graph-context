2a68332a7990fff04bb85af5f3dbf67092b9e9e2
worldwideD <1113657075@qq.com>
2022-05-05 09:03:31 -0400

    chaange loss

--- losses.py
+++ losses.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class ATLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, logits, labels):
+    def forward(self, logits, labels, pos):
+        e_logits = torch.stack([torch.max(logits[st: en, :], dim=0)[0] for st, en in pos]).to(logits)
         # TH label
         th_label = torch.zeros_like(labels, dtype=torch.float).to(labels)
         th_label[:, 0] = 1.0
@@ -17,26 +17,34 @@ class ATLoss(nn.Module):
         n_mask = 1 - labels
 
         # Rank positive classes to TH
-        logit1 = logits - (1 - p_mask) * 1e30
-        loss1 = -(F.log_softmax(logit1, dim=-1) * labels).sum(1)
+        e_logit1 = e_logits - (1 - p_mask) * 1e30
+        loss1 = -(F.log_softmax(e_logit1, dim=-1) * labels).sum(1)
 
         # Rank TH to negative classes
-        logit2 = logits - (1 - n_mask) * 1e30
-        loss2 = -(F.log_softmax(logit2, dim=-1) * th_label).sum(1)
+        #m_n_mask = torch.stack([torch.ones(pos[i, 1]-pos[i, 0], ) * n_mask[i] for i in range(len)], dim=0).to(labels)
+        ep_cnt = len(pos)
+        m_n_mask = torch.cat([n_mask[i:i+1, :].repeat(pos[i, 1]-pos[i, 0], 1) for i in range(ep_cnt)]).to(labels)
+        m_logit2 = logits[pos[0, 0]:, :] - (1 - m_n_mask) * 1e30
+        th_label = torch.zeros_like(m_logit2, dtype=torch.float).to(labels)
+        th_label[:, 0] = 1.0
+        loss2 = -(F.log_softmax(m_logit2, dim=-1) * th_label).sum(1)
 
         # Sum two parts
-        loss = loss1 + loss2
-        loss = loss.mean()
+        #loss = loss1 + loss2
+        #loss = loss.mean()
+        loss = loss1.mean() + loss2.mean()
         return loss
 
-    def get_label(self, logits, num_labels=-1):
-        th_logit = logits[:, 0].unsqueeze(1)
-        output = torch.zeros_like(logits).to(logits)
-        mask = (logits > th_logit)
+    def get_label(self, logits, num_labels=-1, pos = None):
+        e_logits = logits[0: pos[0, 0], :]
+        th_logit = e_logits[: , 0].unsqueeze(1)
+        output = torch.zeros_like(e_logits).to(logits)
+        e_logits = torch.stack([torch.max(logits[st: en, :], dim=0)[0] for st, en in pos]).to(logits)
+        mask = (e_logits > th_logit)
         if num_labels > 0:
-            top_v, _ = torch.topk(logits, num_labels, dim=1)
+            top_v, _ = torch.topk(e_logits, num_labels, dim=1)
             top_v = top_v[:, -1]
-            mask = (logits >= top_v.unsqueeze(1)) & mask
+            mask = (e_logits >= top_v.unsqueeze(1)) & mask
         output[mask] = 1.0
         output[:, 0] = (output.sum(1) == 0.).to(logits)
         return output
--- model.py
+++ model.py
@@ -1,12 +1,9 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from opt_einsum import contract
 from long_seq import process_long_input
 from losses import ATLoss
 
-num_layers = 2
-n_heads = 8
 
 class DocREModel(nn.Module):
     def __init__(self, config, model, emb_size=768, block_size=64, num_labels=-1):
@@ -16,17 +13,14 @@ class DocREModel(nn.Module):
         self.hidden_size = config.hidden_size
         self.loss_fnt = ATLoss()
 
-        self.head_extractor = nn.Linear((num_layers + 1) * config.hidden_size, emb_size)
-        self.tail_extractor = nn.Linear((num_layers + 1) * config.hidden_size, emb_size)
+        self.head_extractor = nn.Linear(2 * config.hidden_size, emb_size)
+        self.tail_extractor = nn.Linear(2 * config.hidden_size, emb_size)
         self.bilinear = nn.Linear(emb_size * block_size, config.num_labels)
 
         self.emb_size = emb_size
         self.block_size = block_size
         self.num_labels = num_labels
 
-        assert config.hidden_size % n_heads == 0
-
-        self.GAT = MultilayersGAT(in_feat=config.hidden_size, nlayers=num_layers, out_feat=config.hidden_size, e_feat=config.hidden_size, nhid=config.hidden_size // n_heads, dropout=0.0, alpha=0.2, nheads=n_heads)
     def encode(self, input_ids, attention_mask):
         config = self.config
         if config.transformer_type == "bert":
@@ -37,111 +31,21 @@ class DocREModel(nn.Module):
             end_tokens = [config.sep_token_id, config.sep_token_id]
         sequence_output, attention = process_long_input(self.model, input_ids, attention_mask, start_tokens, end_tokens)
         return sequence_output, attention
-    
-    def get_graph(self, sequence_output, attention, entity_pos):
-        offset = 1 if self.config.transformer_type in ["bert", "roberta"] else 0
-        m_cnt, e_cnt, d_cnt, m_node, m_sum = 0, 0, len(entity_pos), [], []
-        n, h, _, c = attention.size()
-        # get mention info  
-        for i in range(d_cnt):
-            sum = 0
-            for e in entity_pos[i]:
-                for start, end, sent_id in e:
-                    if start + offset < c:
-                        # In case the entity mention is truncated due to limited max seq length.
-                        m_emb = sequence_output[i, start + offset]
-                        m_att = attention[i, :, start + offset]
-                        m_node.append({'emb': m_emb, 'att': m_att, 'id': m_cnt, 'sent': sent_id, 'entity': e_cnt, 'doc': i})
-                        m_cnt += 1
-                        sum += 1
-                e_cnt += 1
-            m_sum.append(sum)
-        
-        #  build adj matrix and edge id
-        adj, node_features, node_att, edge_id = [], [], [], []
-        for m in m_node:
-            node_features.append(m["emb"])
-            node_att.append(m["att"])
-            _adj = []
-
-            for _m in m_node:
-                if m["doc"] == _m["doc"] and m["sent"] == _m["sent"]: # add a self-loop here
-                    _adj.append(2)
-                else:
-                    _adj.append(int(m["entity"] == _m["entity"]))
-                edge_id.append([m["id"], _m["id"]])
-            
-            for j in range(d_cnt):
-                _adj.append(int(m["doc"] == j))
-                edge_id.append([m["id"], m_cnt+j])
-
-            adj.append(_adj)
-        
-        for i in range(d_cnt):
-            _adj = []
-            
-            for m in m_node:
-                _adj.append(int(m["doc"] == i))
-                edge_id.append([m_cnt+i, m["id"]])
-            for j in range(d_cnt):
-                _adj.append(int(i == j))
-                edge_id.append([m_cnt+i, m_cnt+j])
-            
-            adj.append(_adj)
-            node_features.append(sequence_output[i, 0])
-            att = torch.ones(h, c).to(attention)
-            node_att.append(att)
-        
-        # calculate attention
-        node_att = torch.stack(node_att, dim=0)  # [node_num, h, c]
-        edge_id = torch.LongTensor(edge_id).to(sequence_output.device)  # [node_num*node_num, 2]
-        h_att = torch.index_select(node_att, 0, edge_id[:, 0])  # [node_num*node_num, h, c]
-        t_att = torch.index_select(node_att, 0, edge_id[:, 1])
-        ht_att = (h_att * t_att).mean(1)
-        ht_att = ht_att / (ht_att.sum(1, keepdim=True) + 1e-5)  # [node_num*node_num, c]
-
-        n_node = m_cnt+d_cnt
-        e_features = []
-        ht_att = ht_att.view(n_node, n_node, c)
-        off = 0
-        for i in range(d_cnt):
-            att = []
-            for p in range(m_sum[i]):
-                for q in range(m_sum[i]):
-                    att.append(ht_att[p+off, q+off])
-            att = torch.stack(att, dim=0)
-            ctx = contract("ld,rl->rd", sequence_output[i], att)
-            ctx = ctx.view(m_sum[i], m_sum[i], self.config.hidden_size)
-            for p in range(m_sum[i]):
-                for q in range(n_node):
-                    if q >= off and q < off+m_sum[i]:
-                        e_features.append(ctx[p, q-off])
-                    else:
-                        feat = torch.zeros(self.config.hidden_size).to(sequence_output.device)
-                        e_features.append(feat)
-            off += m_sum[i]
-        for i in range(d_cnt):
-            for j in range(n_node):
-                feat = torch.zeros(self.config.hidden_size).to(sequence_output.device)
-                e_features.append(feat)
-        
-        
-        # return
-        adj_ = torch.tensor(adj).to(sequence_output.device)
-        node_features_ = torch.stack(node_features, dim=0)
-        e_features_ = torch.stack(e_features, dim=0).view(n_node, n_node, self.config.hidden_size)
-        return adj_, node_features_, e_features_
 
     def get_hrt(self, sequence_output, attention, entity_pos, hts):
         offset = 1 if self.config.transformer_type in ["bert", "roberta"] else 0
         n, h, _, c = attention.size()
         hss, tss, rss = [], [], []
+        mp_cnt = 0
+        mp_pos = []
+
+        #   get entity pairs emb
         for i in range(len(entity_pos)):
             entity_embs, entity_atts = [], []
             for e in entity_pos[i]:
                 if len(e) > 1:
                     e_emb, e_att = [], []
-                    for start, end, sent_id in e:
+                    for start, end in e:
                         if start + offset < c:
                             # In case the entity mention is truncated due to limited max seq length.
                             e_emb.append(sequence_output[i, start + offset])
@@ -153,7 +57,7 @@ class DocREModel(nn.Module):
                         e_emb = torch.zeros(self.config.hidden_size).to(sequence_output)
                         e_att = torch.zeros(h, c).to(attention)
                 else:
-                    start, end, sent_id = e[0]
+                    start, end = e[0]
                     if start + offset < c:
                         e_emb = sequence_output[i, start + offset]
                         e_att = attention[i, :, start + offset]
@@ -165,6 +69,7 @@ class DocREModel(nn.Module):
 
             entity_embs = torch.stack(entity_embs, dim=0)  # [n_e, d]
             entity_atts = torch.stack(entity_atts, dim=0)  # [n_e, h, seq_len]
+            mp_cnt += len(hts[i])
 
             ht_i = torch.LongTensor(hts[i]).to(sequence_output.device)
             hs = torch.index_select(entity_embs, 0, ht_i[:, 0])
@@ -178,49 +83,83 @@ class DocREModel(nn.Module):
             hss.append(hs)
             tss.append(ts)
             rss.append(rs)
+
+        ## get mention pairs emb
+        for i in range(len(entity_pos)):
+            #entity_embs, entity_atts = [], []
+            mention_embs, mention_atts = [], []
+            m_cnt = 0
+            pos = []
+            for e in entity_pos[i]:
+                st = m_cnt
+                if len(e) > 1:
+                    #e_emb, e_att = [], []
+                    for start, end in e:
+                        if start + offset < c:
+                            mention_embs.append(sequence_output[i, start + offset])
+                            mention_atts.append(attention[i, :, start + offset])
+                            m_cnt += 1
+                            # In case the entity mention is truncated due to limited max seq length.
+                            #e_emb.append(sequence_output[i, start + offset])
+                            #e_att.append(attention[i, :, start + offset])
+                    #if len(e_emb) > 0:
+                        #e_emb = torch.logsumexp(torch.stack(e_emb, dim=0), dim=0)
+                        #e_att = torch.stack(e_att, dim=0).mean(0)
+                    #else:
+                        #e_emb = torch.zeros(self.config.hidden_size).to(sequence_output)
+                        #e_att = torch.zeros(h, c).to(attention)
+                else:
+                    start, end = e[0]
+                    if start + offset < c:
+                        mention_embs.append(sequence_output[i, start + offset])
+                        mention_atts.append(attention[i, :, start + offset])
+                        m_cnt += 1
+                        #e_emb = sequence_output[i, start + offset]
+                        #e_att = attention[i, :, start + offset]
+                    #else:
+                        #e_emb = torch.zeros(self.config.hidden_size).to(sequence_output)
+                        #e_att = torch.zeros(h, c).to(attention)
+                #entity_embs.append(e_emb)
+                #entity_atts.append(e_att)
+                if st == m_cnt:
+                    mention_embs.append(torch.zeros(self.config.hidden_szie).to(sequence_output))
+                    mention_atts.append(torch.zeros(h, c).to(attention))
+                    m_cnt += 1
+                pos.append([st, m_cnt])
+
+            #entity_embs = torch.stack(entity_embs, dim=0)  # [n_e, d]
+            #entity_atts = torch.stack(entity_atts, dim=0)  # [n_e, h, seq_len]
+            mention_embs = torch.stack(mention_embs, dim=0)
+            mention_atts = torch.stack(mention_atts, dim=0)
+
+            #ht_i = torch.LongTensor(hts[i]).to(sequence_output.device)
+            ht_m = []
+            for eh, et in hts[i]:
+                st = mp_cnt
+                for p in range(pos[eh][0], pos[eh][1]):
+                    for q in range(pos[et][0], pos[et][1]):
+                        ht_m.append([p, q])
+                mp_cnt += (pos[eh][1] - pos[eh][0]) * (pos[et][1] - pos[et][0])
+                mp_pos.append([st, mp_cnt])
+
+            ht_i = torch.LongTensor(ht_m).to(sequence_output.device)
+            hs = torch.index_select(mention_embs, 0, ht_i[:, 0])
+            ts = torch.index_select(mention_embs, 0, ht_i[:, 1])
+
+            h_att = torch.index_select(mention_atts, 0, ht_i[:, 0])
+            t_att = torch.index_select(mention_atts, 0, ht_i[:, 1])
+            ht_att = (h_att * t_att).mean(1)
+            ht_att = ht_att / (ht_att.sum(1, keepdim=True) + 1e-5)
+            rs = contract("ld,rl->rd", sequence_output[i], ht_att)
+            hss.append(hs)
+            tss.append(ts)
+            rss.append(rs)
         hss = torch.cat(hss, dim=0)
         tss = torch.cat(tss, dim=0)
         rss = torch.cat(rss, dim=0)
-        return hss, rss, tss
-    
-    def get_new_ht(self, sequence_output, attention, g_features, entity_pos, hts):
-        offset = 1 if self.config.transformer_type in ["bert", "roberta"] else 0
-        n, h, _, c = attention.size()
-        hss, tss = [], []
-        for i in range(len(entity_pos)):
-            entity_embs = []
-            for e in entity_pos[i]:
-                if len(e) > 1:
-                    e_emb = []
-                    for start, end, sent_id in e:
-                        if start + offset < c:
-                            e_emb.append(g_features[0])
-                            g_features = g_features[1:]
-                    if len(e_emb) > 0:
-                        e_emb = torch.logsumexp(torch.stack(e_emb, dim=0), dim=0)
-                    else:
-                        e_emb = torch.zeros(self.config.hidden_size).to(sequence_output)
-                else:
-                    start, end, sent_id = e[0]
-                    if start + offset < c:
-                        e_emb = g_features[0]
-                        g_features = g_features[1:]
-                    else:
-                        e_emb = torch.zeros(self.config.hidden_size).to(sequence_output)
-                entity_embs.append(e_emb)
+        mp_pos = torch.LongTensor(mp_pos).to(sequence_output.device)
+        return hss, rss, tss, mp_pos
 
-            entity_embs = torch.stack(entity_embs, dim=0)  # [n_e, d]    
-
-            ht_i = torch.LongTensor(hts[i]).to(sequence_output.device)
-            hs = torch.index_select(entity_embs, 0, ht_i[:, 0])
-            ts = torch.index_select(entity_embs, 0, ht_i[:, 1])
-            
-            hss.append(hs)
-            tss.append(ts)
-
-        hss = torch.cat(hss, dim=0)
-        tss = torch.cat(tss, dim=0)
-        return hss, tss
 
     def forward(self,
                 input_ids=None,
@@ -232,105 +171,19 @@ class DocREModel(nn.Module):
                 ):
 
         sequence_output, attention = self.encode(input_ids, attention_mask)
-        adj, g_features, e_features = self.get_graph(sequence_output, attention, entity_pos)
-        g_features = self.GAT(g_features, adj, e_features)
+        hs, rs, ts, pos = self.get_hrt(sequence_output, attention, entity_pos, hts)
 
-        #hs, rs, ts = self.get_hrt(sequence_output, attention, entity_pos, hts)
-        hs, ts = self.get_new_ht(sequence_output, attention, g_features, entity_pos, hts)
-
-        #hs = torch.tanh(self.head_extractor(torch.cat([hs, rs], dim=1)))
-        #ts = torch.tanh(self.tail_extractor(torch.cat([ts, rs], dim=1)))
-        hs = torch.tanh(self.head_extractor(hs))
-        ts = torch.tanh(self.tail_extractor(ts))
+        hs = torch.tanh(self.head_extractor(torch.cat([hs, rs], dim=1)))
+        ts = torch.tanh(self.tail_extractor(torch.cat([ts, rs], dim=1)))
         b1 = hs.view(-1, self.emb_size // self.block_size, self.block_size)
         b2 = ts.view(-1, self.emb_size // self.block_size, self.block_size)
         bl = (b1.unsqueeze(3) * b2.unsqueeze(2)).view(-1, self.emb_size * self.block_size)
         logits = self.bilinear(bl)
 
-        output = (self.loss_fnt.get_label(logits, num_labels=self.num_labels),)
+        output = (self.loss_fnt.get_label(logits, num_labels=self.num_labels, pos=pos),)
         if labels is not None:
             labels = [torch.tensor(label) for label in labels]
             labels = torch.cat(labels, dim=0).to(logits)
-            loss = self.loss_fnt(logits.float(), labels.float())
+            loss = self.loss_fnt(logits.float(), labels.float(), pos)
             output = (loss.to(sequence_output),) + output
         return output
-
-class MultilayersGAT(nn.Module):
-    def __init__(self, in_feat, out_feat, e_feat, nhid, alpha, nheads, dropout=0.0, nlayers=2):
-        super().__init__()
-        self.dropout = dropout
-        self.nlayers = nlayers
-        
-        self.GATlayers = nn.ModuleList([GAT(in_feat, out_feat, nhid, e_feat, dropout=dropout, alpha=alpha, nheads=nheads) for _ in range(nlayers)])
-
-    def forward(self, x, adj, e):
-        h = [x]
-        for GATlayer in self.GATlayers:
-            x = GATlayer(x, adj, e)
-            h.append(x)
-        h = torch.cat(h, dim=1)
-
-        return h
-
-class GAT(nn.Module):
-    def __init__(self, in_features, out_features, hid_features, e_features, alpha, nheads, dropout=0.0):
-        super().__init__()
-        self.dropout = dropout
-        self.nheads = nheads
-        self.out_features = out_features
-
-        self.attns = nn.ModuleList([GraphAttentionLayer(in_features, hid_features, e_features, dropout=dropout, alpha=alpha) for _ in range(nheads)])
-        #self.W = nn.Parameter(torch.zeros(size=(nheads * hid_features, out_features, )))
-        #nn.init.xavier_uniform_(self.W.data, gain=1.414)
-    
-    def forward(self, x, adj, e):
-        #x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.cat([att(x, adj, e) for att in self.attns], dim=1)
-        #x = F.dropout(x, self.dropout, training=self.training)
-        
-        N = x.size()[0]
-        #y = x.view(N, self.nheads, self.out_features).mean(1)
-        #y = F.elu(torch.matmul(x, self.W))
-        x = F.elu(x)
-
-        return x
-
-class GraphAttentionLayer(nn.Module):
-    def __init__(self, in_features, out_features, e_features, alpha, dropout=0.0):
-        super().__init__()
-        self.dropout = dropout
-        self.in_features = in_features
-        self.out_features = out_features
-        self.e_features = e_features
-        self.alpha = alpha
-
-        self.W_q = nn.Parameter(torch.zeros(size=(in_features, out_features, )))
-        nn.init.xavier_uniform_(self.W_k.data, gain=1.414)
-        self.W_k = nn.Parameter(torch.zeros(size=(in_features + e_features, out_features, )))
-        nn.init.xavier_uniform_(self.W_qe.data, gain=1.414)
-        self.W_v = nn.Parameter(torch.zeros(size=(in_features + e_features, out_features, )))
-        nn.init.xavier_uniform_(self.W_q.data, gain=1.414)
-        self.a = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
-        nn.init.xavier_uniform_(self.a.data, gain=1.414)
-        self.leakyrelu = nn.LeakyReLU(self.alpha)
-    
-    def forward(self, input, adj, e_feat):
-        hq = torch.mm(input, self.W_q)
-        N = hq.size()[0]
-        hk = torch.cat([input.repeat(N, 1), e_feat.view(N * N, -1)], dim=1)
-        hv = torch.matmul(hk, self.W_v)
-        hk = torch.matmul(hk, self.W_k)
-
-        a_input = torch.cat([hq.repeat(1, N).view(N * N, -1), hk], dim=1).view(N, -1, 2 * self.out_features)
-        e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
-
-        zero_vec = -9e15*torch.ones_like(e)
-        
-        att = torch.where(adj > 0, e, zero_vec)
-        att = F.softmax(att, dim=1).unsqueeze(1)
-        hv = hv.view(N, N, -1)
-        h_prime = torch.matmul(att, hv).squeeze(1)
-        #h_prime = torch.stack([torch.matmul(att[i], hv[i]) for i in range(N)], dim=0).squeeze(1)
-        #h_prime = h_prime + hq
-
-        return h_prime
